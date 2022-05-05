4b64177b970801f2f41c18ce49b03809db14aaa7
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2022-05-05 11:22:29 +0200

    update master

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Footer';
 import Home from "./Home";
 import Header from "./Header";
