6c6a2fb7cb55a7772352eb8e4c2beb641a5144a3
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2022-05-05 09:28:09 +0200

    correction erreurs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/App.css';
 import Home from "./Home";
-import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
         <Home/>
-        <Search/>
     </div>
   );
 }
